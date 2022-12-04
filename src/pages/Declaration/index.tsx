3ab92bab98917d0c7429b6cc6dce58002fe7980e
hai0111 <105141081+hai0111@users.noreply.github.com>
2022-12-04 18:04:15 +0700

    update

--- src/pages/Declaration/index.tsx
+++ src/pages/Declaration/index.tsx
@@ -1,7 +1,33 @@
-import { useFormik } from 'formik'
+import { FormikHelpers, useFormik } from 'formik'
+import React from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
-import { IItem } from '../../components/TableComponent'
 import { v4 as uuidv4 } from 'uuid'
+import { IItem } from '../../components/TableComponent'
+import * as Yup from 'yup'
+
+interface ITravel {
+	departure?: string
+	departureDate?: string
+	destination?: string
+	immigrationDate?: string
+}
+
+interface IItemErrors {
+	address?: string
+	dateOfBirth?: string
+	district?: string
+	email?: string
+	fullName?: string
+	gender?: string
+	id?: string
+	mobile?: string
+	nationId?: string
+	nationality?: string
+	object?: string
+	province?: string
+	symptoms?: string[]
+	travels?: ITravel[]
+}
 
 const Declaration = () => {
 	const initValues: IItem = {
@@ -28,14 +54,32 @@ const Declaration = () => {
 		]
 	}
 
-	const handleSubmit = (e: any) => {
-		console.log(e)
+	const handleSubmit = (values: IItem, formikHelpers: FormikHelpers<any>) => {}
+	const validate = (values: IItem) => {
+		const errors: IItemErrors = {}
+		if (values.fullName === '') {
+			errors.fullName = 'Name is required'
+		}
+		if (values.object === '') {
+			errors.object = 'Object is required'
+		}
+		if (values.dateOfBirth === '') {
+			errors.dateOfBirth = 'Date of birth is required'
+		}
+		return errors
 	}
 
 	const formik = useFormik({
 		initialValues: { ...initValues },
-		onSubmit: handleSubmit
+		onSubmit: handleSubmit,
+		validate
+		// validateOnBlur: true,
+		// validateOnChange: true
 	})
+
+	React.useEffect(() => {
+		console.log(formik.values)
+	}, [formik.values])
 
 	return (
 		<Container>
@@ -45,20 +89,59 @@ const Declaration = () => {
 			>
 				MEDICAL DECLARATION FORM FOR FOREIGN ENTRY
 			</h1>
-			<Form>
-				<Form.Group className="mb-3" controlId="formBasicEmail">
+			<Form onSubmit={formik.handleSubmit}>
+				<Form.Group className="mb-3">
 					<Form.Label>
 						Full name <span className="text-danger">*</span>
 					</Form.Label>
-					<Form.Control placeholder="Full name..." />
-					<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+					<Form.Control
+						placeholder="Full name..."
+						name="fullName"
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.fullName}
+						isInvalid={!!formik.errors.fullName}
+					/>
+					<Form.Control.Feedback type="invalid">
+						{formik.errors.fullName}
+					</Form.Control.Feedback>
 				</Form.Group>
-				<Form.Group className="mb-3" controlId="formBasicEmail">
+				<Form.Group className="mb-3">
 					<Form.Label>
-						Full name <span className="text-danger">*</span>
+						Object <span className="text-danger">*</span>
 					</Form.Label>
-					<Form.Control placeholder="Full name..." />
-					<Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+					<Form.Select
+						name="object"
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.object}
+						isInvalid={!!formik.errors.object}
+					>
+						<option value="">------Choose</option>
+						<option value="Expert">Expert</option>
+						<option value="VietNamese">VietNamese</option>
+						<option value="International Student">International Student</option>
+						<option value="Other">Other</option>
+					</Form.Select>
+					<Form.Control.Feedback type="invalid">
+						{formik.errors.object}
+					</Form.Control.Feedback>
+				</Form.Group>
+				<Form.Group className="mb-3">
+					<Form.Label>
+						Date of birth <span className="text-danger">*</span>
+					</Form.Label>
+					<Form.Control
+						type="date"
+						name="dateOfBirth"
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.dateOfBirth}
+						isInvalid={!!formik.errors.dateOfBirth}
+					/>
+					<Form.Control.Feedback type="invalid">
+						{formik.errors.dateOfBirth}
+					</Form.Control.Feedback>
 				</Form.Group>
 				<Button variant="primary" type="submit">
 					Submit
